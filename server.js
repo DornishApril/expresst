b1241dd7732495075c860ca0d2f45d56386c2bb0
DornishApril <samireformed786@gmail.com>
2025-06-05 15:49:31 +0600

    upto 87

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = require('./app');
 //console.log(app.get('env'));
 //console.log(process.env);
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DB_PASSWORD,
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
 console.log(DB);
 
 mongoose
@@ -19,10 +16,29 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then((con) => {
-    console.log(con.connections);
+  .then(() => {
     console.log('DB connection successful');
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name!'],
+    unique:true
+  },
+  rating: {
+    type: Number,
+    default:4.5
+  },
+  price: {
+    type: Number,
+    required:[true,'A tour must have a price']
+  }
+});
+
+ 
+const Tour = mongoose.model('Tour',tourSchema);
+
 
 const PORT = process.env.PORT || 3000;
 
