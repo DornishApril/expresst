e6d5d64f8af5c09a1bca5752921a96b74f69e3e2
DornishApril <samireformed786@gmail.com>
2025-06-05 15:14:23 +0600

    db connected successful

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -6,9 +7,25 @@ const app = require('./app');
 //console.log(app.get('env'));
 //console.log(process.env);
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD,
+);
+console.log(DB);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+  });
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
-
